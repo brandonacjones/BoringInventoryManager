90b93e460cc559b77fbce51c4ae71c9afb3814b0
brandonacjones <brandon.ac.jones@gmail.com>
2025-09-07 21:39:37 -0700

    Implemented basic search functionality to log window.

--- src/main/java/controller/LogController.java
+++ src/main/java/controller/LogController.java
@@ -3,15 +3,18 @@ package controller;
 import dao.LogsDAO;
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
+import javafx.collections.transformation.FilteredList;
 import javafx.fxml.FXML;
 import javafx.scene.Scene;
 import javafx.scene.control.TableColumn;
 import javafx.scene.control.TableView;
+import javafx.scene.control.TextField;
 import javafx.scene.control.cell.PropertyValueFactory;
 import model.LogItem;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.logging.Filter;
 
 
 public class LogController {
@@ -21,6 +24,7 @@ public class LogController {
     @FXML private TableColumn<LogItem, String> fx_typecol;
     @FXML private TableColumn<LogItem, String> fx_usercol;
     @FXML private TableColumn<LogItem, String> fx_descriptioncol;
+    @FXML private TextField fx_logfield;
 
     private LogsDAO logsDAO = new LogsDAO();
     private Scene scene;
@@ -38,5 +42,22 @@ public class LogController {
         fx_usercol.setCellValueFactory(new PropertyValueFactory<>("user"));
         fx_descriptioncol.setCellValueFactory(new PropertyValueFactory<>("description"));
         fx_logtable.setItems(FXCollections.observableArrayList(logItems));
+    }
+
+    @FXML
+    public void search() {
+        ObservableList<LogItem> original = FXCollections.observableList(logItems);
+        FilteredList<LogItem> filtered = new FilteredList<>(original, logItem -> true);
+
+        String query = fx_logfield.getText().trim().toLowerCase();
+
+        filtered.setPredicate(logItem ->
+            query.isEmpty() ||
+                    logItem.getType().toLowerCase().contains(query) ||
+                    logItem.getDescription().toLowerCase().contains(query) ||
+                    logItem.getUser().contains(query)
+        );
+
+        fx_logtable.setItems(filtered);
     }
 }
